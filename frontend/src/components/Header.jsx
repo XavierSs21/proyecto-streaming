604179e8565d9be39f385a938f6384c9fc636f93
Pal0mafd <fernandezmayra302@gmail.com>
2025-12-10 20:16:42 -0600

    changes on the ui were made

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,43 @@
-import { Button } from "./ui/button"
+// import { useState } from "react"
+import SearchBar from "./SearchBar"
 
+import { Button } from "./ui/button"
+// import DialogSession from "./DialogSession"
 
 const Header = () => {
-  return (
-    <div className="flex justify-between border-b p-6">
-      <Button>Iniciar Sesion</Button>
-    </div>
+  // const [open, setOpen] = useState(false)
+  // const [mode, setMode] = useState("login") // login | register
 
+  return (
+    <div className="flex justify-between items-center border-b p-6 gap-6">
+      <h1>MUVI</h1>
+
+      <SearchBar />
+
+      <h2>Mis Listas</h2>
+
+      <Button
+        className="rounded-3xl"
+        onClick={() => {
+          setMode("login")
+          setOpen(true)
+        }}
+      >
+        Iniciar Sesión
+      </Button>
+
+      <Button
+        className="rounded-3xl bg-white border black text-black"
+        onClick={() => {
+          setMode("register")
+          setOpen(true)
+        }}
+      >
+        Crear una cuenta
+      </Button>
+
+      {/* <DialogSession open={open} setOpen={setOpen} mode={mode} /> */}
+    </div>
   )
 }
 
