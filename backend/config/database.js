c7bf921f2933d0c5e287a4c851ffa58fa2ac88b0
XavierSs21 <xavier.sotomayor21@hotmail.com>
2025-12-14 22:43:48 -0600

    chore(backend): update database connection logging

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
         console.log(`Mongo conectado! ${conn.connection.host}`);
+        console.log(`Base de datos: ${conn.connection.name}`);
     } catch (e) {
         console.log(`error: ${e.message}`);
         process.exit(1);
